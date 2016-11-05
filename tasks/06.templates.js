ec4a85c08ee35f712ae5e62ea7e1f8d7e17c4f92
wallen <wallen_weel@outlook.com>
2016-11-05 23:02:49 +0800

    fix templates task filter issue

--- tasks/06.templates.js
+++ tasks/06.templates.js
@@ -7,41 +7,39 @@ import plumber from 'gulp-plumber'
 export default (
   { task, src, dest, changed, $if },
   c,
-  cfg
+  { getValidFiles, path, env, styles, scripts }
 ) => {
-  const { getValidFiles, path, env, styles, scripts } = cfg
-
   /**
    * Getting Files'name of A Path
    * @param  {String} type the name in config's origin property
    * @return {Array}       a collection of these files's name, no postfix
    */
   const getFilesName = type =>
-    getValidFiles(path.src(cfg[type].cwd.src)).map(k => k.replace(/\.\w+$/, ''))
+    getValidFiles(path.src(type.cwd.src)).map(k => k.replace(/\.\w+$/, ''))
 
-  const stylesFilenames = getFilesName('styles').join('|')
+  const stylesFilenames = getFilesName(styles).join('|')
   const stylesReg = new RegExp(`(\\<link.+href\\=[\\'\\"].*)(${stylesFilenames})(\\.css[\\'\\"].*\\>)`, 'g')
 
-  const scriptsFilenames = getFilesName('scripts').join('|')
+  const scriptsFilenames = getFilesName(scripts).join('|')
   const scriptsReg = new RegExp(`(\\<script.+src\\=[\\'\\"].*)(${scriptsFilenames})(\\.js[\\'\\"].*\\>\\<\\/script\\>)`, 'g')
 
-  const pugFilter = filter('**/*.pug', { restore: true })
-  const htmlFilter = filter('**/*.html')
+  task('templates', async () => {
+    const pugFilter = filter('**/*.pug', { restore: true })
+    const htmlFilter = filter('**/*.html')
 
-  task('templates', async () => await src(c.src)
-    .pipe(plumber())
+    return await src(c.src)
+      .pipe(plumber())
 
-    .pipe(pugFilter)
-    .pipe(pug(c.pug))
-    .pipe(pugFilter.restore)
+      .pipe(pugFilter)
+      .pipe(pug(c.pug))
+      .pipe(pugFilter.restore)
 
-    .pipe(htmlFilter)
+      .pipe(htmlFilter)
 
-    .pipe($if(env.prod, replace(stylesReg, '$1$2' + styles.min + '$3')))
-    .pipe($if(env.prod, replace(scriptsReg, '$1$2' + scripts.min + '$3')))
+      .pipe($if(env.prod, replace(stylesReg, '$1$2' + styles.min + '$3')))
+      .pipe($if(env.prod, replace(scriptsReg, '$1$2' + scripts.min + '$3')))
 
-    .pipe(changed(c.dest))
-    .pipe(dest(c.dest))
-  )
-
+      .pipe(changed(c.dest))
+      .pipe(dest(c.dest))
+  })
 }
